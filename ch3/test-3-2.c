84dd1d8b3bb2b0552e1aaf34614ebf0387e48b8b
timtingwei <timtingwei@hotmail.com>
2018-08-02 17:40:58 +0800

    测试行编辑程序

--- ch3/test-3-2.c
+++ ch3/test-3-2.c
@@ -1,9 +1,13 @@
 // copyright [2018] <mituh>
 // test-3-2.c
 
-#include "conversion.c"
+// #include "conversion.c"
+// #include "brackets_match.c"
+#include "lineEdit.c"
 
 int main() {
-  conversion();
+  // conversion();
+  // printf("result = %d\n", brackets_match());
+  lineEdit();
   return 0;
 }
