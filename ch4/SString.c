a95948879739673be8c021faafae2275f1dc63cf
timtingwei <timtingwei@hotmail.com>
2018-08-03 19:02:43 +0800

    实现连接串Concat和求子串SubString操作

--- ch4/SString.c
+++ ch4/SString.c
@@ -16,8 +16,44 @@ typedef int Status;      // Status是函数返回的值
 
 // = = = = = ADT SString 的表示与实现  = = = = =
 
+// - - - - - 串的定义顺序存储表示 - - - - -
+#define MAXSTRLEN 255       // 用户可以在255以内定义最大串长
+typedef unsigned char SString[MAXSTRLEN];     // 字符数组
 
+// 用T返回由S1, S2连接而成的新串, 若未截断, 返回TRUE, 否则返回FALSE
+Status Concat(SString *T, SString S1, SString S2) {
+  int uncut; int i;                      // 截断标记, 循环计数
+  if (S1[0] + S2[0] <= MAXSTRLEN) {      // 未截断
+    (*T)[0] = S1[0] + S2[0];
+    for (i = 1; i <= S1[0]; i++) { (*T)[i] = S1[i];}
+    for (i = 1; i <= S2[0]; i++) { (*T)[i+S1[0]] = S2[i];}
+    uncut = TRUE;
+  } else if (S1[0] < MAXSTRLEN) {        // S2被截断
+    (*T)[0] = MAXSTRLEN;
+    for (i = 1; i <= S1[0]; i++) { (*T)[i] = S1[i];}
+    for (i = 1; i <= MAXSTRLEN - S1[0]; i++) { (*T)[i+S1[0]] = S2[i]; }
+    uncut = FALSE;
+  } else {                               // 截断(仅取S1)
+    (*T)[0] = S1[0];
+    for (i = 1; i <= S1[0]; i++) { (*T)[i] = S1[i];}
+    uncut = FALSE;
+  }
+  return uncut;
+}  // Concat
 
+// 用Sub返回串S的第pos个字符起长度为len的子串
+Status SubString(SString *Sub, SString S, int pos, int len) {
+  // if (pos+len-1 > S[0]) return ERROR;      // 输入长度和位置不合法
+  if (pos < 1 || pos > S[0] || len < 0 || len > S[0] - pos + 1)
+    return ERROR;                             // 输入长度和位置不合法
+  int i;
+  for (i = 1; i <= len; i++) {
+    (*Sub)[i] = S[i+pos-1];
+  }
+  (*Sub)[0] = len; return OK;
+}  // SubString
+
+/*
 int Index(String S, String T, int pos) {
   // T为非空串. 若主串S中第pos个字符之后存在与T相等的子串
   int m, n, i; String sub;
@@ -32,9 +68,9 @@ int Index(String S, String T, int pos) {
   }  // if
   return 0;                          // 不存在子串T, 返回0
 }  // Index
-
+*/
 
 int main() {
-  
+  SString str = "dsds";
   return 0;
 }
