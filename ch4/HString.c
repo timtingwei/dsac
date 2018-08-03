71b84c05c1185e76403f257c6acfcd802dc614cc
timtingwei <timtingwei@hotmail.com>
2018-08-04 03:09:01 +0800

    strAssign

--- ch4/HString.c
+++ ch4/HString.c
@@ -22,6 +22,37 @@ typedef struct {
   int    length;   // 串长度
 }HString;
 
+// - - - - - 基本操作的函数原型说明 - - - - -
+Status StrAssign(HString *T, char * chars);
+  // 生成一个其值等于串常量char的串T
+int StrLength(HString S);
+  // 返回S的元素个数, 串长
+int StrCompare(HString S, HString T);
+  // 若S>T, 则返回值>0; S=T, 返回值=0; S<T, 返回值<0
+Status ClearString(HString *S);
+  // 将s清为空串, 并释放S的空间
+Status Concat(HString *S, HString *T);
+  // 用T返回由S1和S2联接而成的新串
+HString SubString(HString S, int pos, int len);
+  // 返回串S从第pos个字符起长度为len的子串
+
+// 生成一个其值等于串常量char的串T(别忘记为T的存储区分配空间, 判断常量串?为空串)
+Status StrAssign(HString *T, char * chars) {
+  int i; char *c;
+  for (i = 0, c = chars; *c; i++, c++) {}               // 求串chars的长度
+  if (!i) {
+    (*T).ch = NULL; (*T).length = 0;                    // 常量串是空串
+  } else {
+    int j;
+    // 千万记得为T.ch分配存储空间
+    if (!((*T).ch = (char*) malloc (i * sizeof(char))))
+      return exit(OVERFLOW);
+    for (j = 0; j <= i-1; j++) (*T).ch[j] = chars[j];   // 将常量串赋值给T
+    (*T).length = i;                                    // 别忘了T的长度
+  }
+  return OK;
+}
+
 // 在串S的pos个字符之前插入T
 Status StrInsert(HString* S, int pos, HString T) {
   // 1<=pos<=S.length+1, 插入位置合理
