2b0e0b9db89f591ac034be7fd7790cffea55a11c
timtingwei <timtingwei@hotmail.com>
2018-08-06 00:27:36 +0800

    建立二叉树

---
+++ ch6/CreateBiTree.c
@@ -0,0 +1,50 @@
+// Copyright [2018] <mituh>
+// CreateBiTree.c
+// 建立二叉树
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+typedef int Status;      // Status是函数返回的值
+
+
+
+typedef char TElemType;
+
+/* 二叉树的二叉链表结点结构定义 */
+typedef struct BiTNode {
+  TElemType data;
+  struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+
+// 中序遍历建立二叉树
+void CreateBiTree(BiTree *T) {
+  TElemType ch;
+  scanf("%c", &ch);
+  if (ch == '#') {
+    *T = NULL;
+  } else {
+    *T = (BiTree) malloc (sizeof(BiTNode));
+    if (!*T) exit(OVERFLOW);
+    (*T) -> data = ch;
+    printf("data=%c\n", (*T)->data);
+    CreateBiTree(&((*T)->lchild));   /* 构造左子树 */
+    CreateBiTree(&((*T)->rchild));   /* 构造右子树 */
+  }
+}
+// AB#D##C##
+// #B#D#A#C#
+// ###DB##CA
+
+int main() {
+  BiTree T;
+  CreateBiTree(&T);
+  return 0;
+}
