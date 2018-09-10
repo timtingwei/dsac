9c7dd22228fd06992733dc27622e4d05c9a336a4
timtingwei <timtingwei@hotmail.com>
2018-09-10 22:58:03 +0800

    Update: 循环和递归打印1~n

--- zju_dsac/README.md
+++ zju_dsac/README.md
@@ -3,3 +3,54 @@
 ## 1. 概论
 
 ### 例2-printN
+
+循环实现的函数
+```cpp
+#include <cstdio>
+int main() {
+  int N; scanf("%d", &N);
+  int i;
+  for (i = 1; i <= N; i++) {
+    printf("%d\n", i);
+  }
+  return 0;
+}
+```
+
+
+自己第一遍写的递归
+```cpp
+#include <cstdio>
+
+void printN(int n) {
+  if (n == 1) {
+    printf("%d\n", n);
+    return;
+  }
+  printN(n-1); printf("%d\n", n);
+}
+
+int main() {
+  int N; scanf("%d", &N);
+  printN(N);
+  return 0;
+}
+```
+
+简化递归函数
+```cpp
+#include <cstdio>
+
+void printN(int n) {
+  if (n) {
+    printN(n-1); printf("%d\n", n);
+  }
+  return;
+}
+
+int main() {
+  int N; scanf("%d", &N);
+  printN(N);
+  return 0;
+}
+```
--- zju_dsac/ch1/printN.cpp
+++ zju_dsac/ch1/printN.cpp
@@ -2,19 +2,19 @@
 // printN.cpp
 
 /*
+// 循环的写法
 #include <cstdio>
-int main() {
-  int N; scanf("%d", &N);
+void printN(int n) {
   int i;
   for (i = 1; i <= N; i++) {
     printf("%d\n", i);
   }
-  return 0;
 }
 */
 
+/*
+// 自己写的递归
 #include <cstdio>
-
 void printN(int n) {
   if (n == 1) {
     printf("%d\n", n);
@@ -22,9 +22,48 @@ void printN(int n) {
   }
   printN(n-1); printf("%d\n", n);
 }
+*/
 
+/*
+// 更新递归
+void printN(int n) {
+  if (n) {
+    printN(n-1); printf("%d\n", n);
+  }
+  return;
+}
+*/
+
+#include <cstdio>
+#include <ctime>
+
+void printN_loop(int n) {
+  int i;
+  for (i = 1; i <= n; i++) {
+    printf("%d\n", i);
+  }
+}
+
+void printN_recursive(int n) {
+  if (n) {
+    printN_recursive(n-1); printf("%d\n", n);
+  }
+  return;
+}
+
+clock_t first, second, third;
+double duration1, duration2;
 int main() {
   int N; scanf("%d", &N);
-  printN(N);
+  first = clock();
+  printN_loop(N);
+  second = clock();
+  printN_recursive(N);
+  third = clock();
+
+  duration1 = ((double)(second-first))/CLOCKS_PER_SEC;   // CLK_TCK
+  duration2 = ((double)(third-second))/CLOCKS_PER_SEC;
+  printf("printN_loop(), N = %d, time = %.3lf\n", N, duration1);
+  printf("printN_recursive(), N = %d, time = %.3lf\n", N, duration2);
   return 0;
 }
