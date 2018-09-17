604c7d7cb743eeb9e9fb8ed357c3a58bc665b75d
timtingwei <timtingwei@hotmail.com>
2018-09-17 10:11:50 +0800

    Update: 修改成带头结点的版本

--- zju_dsac/exercise/02-1_link-list-merge.c
+++ zju_dsac/exercise/02-1_link-list-merge.c
@@ -1,6 +1,29 @@
 // Copyright[2018] <mituh>
-// 02-1_link-list-merge.cpp
+// 02-1_link-list-merge.c
 
+List Merge(List L1, List L2) {
+  List L;
+  L = (List)malloc(sizeof(struct Node));
+  List pa = L1->Next, pb = L2->Next, pc = L;
+  while (pa && pb) {
+    if (pa->Data <= pb->Data) {
+      pc->Next = pa;
+      pc = pa;
+      pa = pa->Next;
+    } else {
+      pc->Next = pb;
+      pc = pb;
+      pb = pb->Next;
+    }
+  }
+  if (pa) pc->Next = pa;
+  if (pb) pc->Next = pb;
+  L1->Next = NULL; L2->Next = NULL;
+  return L;
+}
+
+
+/*
 List Merge(List L1, List L2) {
   List L;
   L = (List)malloc(sizeof(struct Node));
@@ -29,3 +52,4 @@ List Merge(List L1, List L2) {
   L1 = NULL; L2 = NULL;
   return L;
 }
+*/
