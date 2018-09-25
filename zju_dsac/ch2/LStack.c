9730155de2a7d016b99e072b5e6e4480ef02c19b
timtingwei <timtingwei@hotmail.com>
2018-09-25 19:56:42 +0800

    Update: 实现带头结点链栈的入栈和出栈

--- zju_dsac/ch2/LStack.c
+++ zju_dsac/ch2/LStack.c
@@ -24,6 +24,7 @@ int isEmpty(LStack S) {
   return (S->Next == NULL);
 }
 
+/*
 void push(LStack S, ElementType elem) {
   ptrToNode node;
   node = (ptrToNode)malloc(sizeof(struct SNode));
@@ -42,4 +43,27 @@ int pop(LStack S, ElementType *elem) {
   free(temp);
   return OK;
 }
+*/
 
+
+// 带头结点的入栈
+void push(LStack S, ElementType elem) {
+  struct SNode *node;
+  node = (struct SNode*)malloc(sizeof(struct SNode));
+  node->Next = S->Next;
+  node->Data = elem;
+  S->Next = node;
+}
+
+#define ERROR -1
+// 带头结点的出栈
+ElementType pop(LStack S) {
+  if (isEmpty(S)) return ERROR;
+  struct SNode *firstCell;
+  ElementType topElem;
+  firstCell = S->Next;
+  S->Next = firstCell->Next;
+  topElem = firstCell->Data;
+  free(firstCell);
+  return topElem;
+}
