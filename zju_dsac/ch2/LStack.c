08b54376856c60d85f2a0d5ed6c5ffeaff864f6e
timtingwei <timtingwei@hotmail.com>
2018-09-25 19:31:02 +0800

    Update: 实现入栈和出栈操作

--- zju_dsac/ch2/LStack.c
+++ zju_dsac/ch2/LStack.c
@@ -2,6 +2,8 @@
 // LStack.c
 // 链表实现栈
 
+#include <stdlib.h>
+
 typedef int ElementType;
 
 typedef struct SNode *ptrToNode;
@@ -9,7 +11,7 @@ struct SNode {
   ElementType Data;
   ptrToNode Next;
 };
-typedef ptrToNode LStack
+typedef ptrToNode LStack;
 
 LStack createEmpty() {
   LStack S;
@@ -22,4 +24,22 @@ int isEmpty(LStack S) {
   return (S->Next == NULL);
 }
 
+void push(LStack S, ElementType elem) {
+  ptrToNode node;
+  node = (ptrToNode)malloc(sizeof(struct SNode));
+  node->Data = elem;
+  node->Next = S;
+  S = node;
+}
+
+#define OK 1
+#define ERROR 0
+int pop(LStack S, ElementType *elem) {
+  if (isEmpty(S)) return ERROR;
+  LStack temp = S;
+  *elem = temp->Data;
+  S = S->Next;
+  free(temp);
+  return OK;
+}
 
