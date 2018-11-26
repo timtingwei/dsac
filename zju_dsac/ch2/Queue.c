bd43d0439d9e155eca038b1fda5f5bd8d217096d
timtingwei <timtingwei@hotmail.com>
2018-11-26 15:19:54 +0800

    Update: 增加队列的构造方法

---
+++ zju_dsac/ch2/Queue.c
@@ -0,0 +1,31 @@
+// Copyright [2018] <mituh>
+// Queue.c
+// 队列的构造
+
+#include <stdio.h>
+#include <stdlib.h>
+
+Queue CreateQueue(int maxSize) {
+  // 生成长度为maxSize的空队列
+  return queue
+}
+
+int IsFullQ(Queue Q, int maxSize) {
+  // 判断队列Q是否已经满
+  return result
+}
+
+void AddQ(Queue Q, ElementType item) {
+  // 将数据元素item插入队列Q中
+  return
+}
+
+int IsEmpty(Queue Q) {
+  // 判断队列是否为空
+  return result
+}
+
+ElementType DeleteQ(Queue Q) {
+  // 将队头数据删除并返回
+  return item
+}
