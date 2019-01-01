d2dbb62c60b49b7fe101e4004fdddf3e158f99db
timtingwei <timtingwei@hotmail.com>
2019-01-01 22:40:14 +0800

    Update: 后序遍历求二叉树的高度

--- zju_dsac/ch3/BinTreeTraversal.c
+++ zju_dsac/ch3/BinTreeTraversal.c
@@ -75,3 +75,15 @@ void PreOrderPrintLeaves(BinTree BT) {
   }
 }
 
+/* 求二叉树的高度 */
+int PostOrderGetHeight(BinTree BT) {
+  int HL, HR, MaxH;
+  if (BT) {
+    HL = PostOrderGetHeight(BT->Left);
+    HR = PostOrderGetHeight(BT->Right);
+    MaxH = (HL > HR) ? HL : HR;
+    return (MaxH + 1);   /* 带入根结点高度得到树高 */
+  } else {
+    return 0;    /* 空树树高为0 */
+  }
+}
