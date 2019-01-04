0e20ee31f7ccd9d0c46b801dd366238a7ae311df
timtingwei <timtingwei@hotmail.com>
2019-01-04 19:54:53 +0800

    Update: 简化成别插入边排堆的思路, 定义成全局

--- zju_dsac/ch3/ex-PrintPath.c
+++ zju_dsac/ch3/ex-PrintPath.c
@@ -16,87 +16,46 @@ output:
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-
-#define ElementType int
-typedef struct HeapNode *Heap;
-struct HeapNode {
-  ElementType *Elements;
-  int Size;
-  int Capacity;
-};
-typedef Heap MinHeap;
-
-void Insert(MinHeap H, ElementType item) {
-  /* 将元素插入到堆的最后一个结点 */
-  H->Elements[++H->Size] = item;
-}
-
+/* 定义成全局, 不用重新定义一个堆了 */
 #define MAXD -1001    /* 哨兵 */
 #define MAXN 1000     /* 堆的最大容量 */
-void CreateHeap(MinHeap H, int MaxSize) {
-  /* 创建并读入未排序的堆 */
-  ElementType item;
-  H->Elements = (ElementType *)malloc(sizeof(ElementType) * (MaxSize + 1));
-  H->Elements[0] = MAXD;
-  H->Size = 0;
-  H->Capacity = MAXN;
-  while (MaxSize--) {
-    scanf("%d", &item);
-    Insert(H, item);
-  }
-  return H;
+
+int H[MAXN], size;    /* 堆的数组和容量定义成全局! */
+
+void Create() {
+  size = 0;
+  H[0] = MAXD;
 }
 
-void PrecUp(MinHeap H, int p) {
-  /* 过滤: 将H以p下标结点为根的子树排成最小堆 */
-  ElementType X;
-  int Parent, Child;
-  X = H->Elements[p];
-
-  for (Parent = p; Parent*2 <= H->Size; Parent = Child) {
-    Child = Parent * 2;
-    if ((Child != H->Size) &&
-      (H->Elements[Child] > H->Elements[Child+1]))
-      Child++;
-    if (X <= H->Elements[Child]) {
-      break;
-    } else {
-      H->Elements[Parent] = H->Elements[Child];
-    }
-  }
-  H->Elements[Parent] = X;
-}
-
-void BuildHeap(MinHeap H) {
-  /* 根据已经放好的堆, 建立最小堆 */
+void Insert(int X) {
+  /* 将X插入到堆中, 要实时保持最小堆! */
   int i;
-  for (i = H->Size/2; i > 0; i /= 2) {
-    PrecUp(H, i);
+  for (i = ++size; H[i/2] > X; i /= 2) {
+    H[i] = H[i/2];
   }
-}
-
-void PrintPath(MinHeap H, int p) {
-  /* 打印从下标p的结点到根结点的路径 */
-  int i;
-  printf("%d ", H->Elements[p]);
-  for (i = p/2; i > 0; i /= 2) {
-    printf(" %d", H->Elements[i]);
-  }
-  print("\n");
+  H[i] = X;
 }
 
 int main() {
-  int n, m, p;
-  scanf("%d %d\n", &n, &m);
-  MinHeap H;
-  CreateHeap(H, n);    /* 创建并读入未排序的堆 */
-  BuildHeap(H);         /* 将堆建立成小顶堆 */
-
-  while (m--) {
-    scanf("%d", &p);    /* 读入结点下标 */
-    PrintPath(H, p);    /* 打印从结点到根的路径 */
+  int n, m, i, x, p;
+  scanf("%d %d", &n, &m);   /* scanf不需要换行 */
+  Create();    /* 创建并读入未排序的堆 */
+  for (i = 0; i < n; i++) {
+    scanf("%d", &x);
+    Insert(x);
   }
+  // BuildHeap(H);         /* 将堆建立成小顶堆 */   用的是创建空堆插入的方法!
+
+  for (i = 0; i < m; i++) {
+    scanf("%d", &p);    /* 读入结点下标 */
+    printf("%d", H[p]);
+    while (p > 1) {
+      p /= 2;
+      printf(" %d", H[p]);
+    }
+    printf("\n");
+  }
+  
   return 0;
 }
