b0dfdd25f807cb4be677fa853f53e106fbc33cd9
timtingwei <timtingwei@hotmail.com>
2019-01-04 21:45:09 +0800

    Update: 直接用数组中的值作为父结点的下标

--- zju_dsac/ch3/FileTransfer.c
+++ zju_dsac/ch3/FileTransfer.c
@@ -16,3 +16,20 @@ int Find(SetType S[], ElementType X) {
   for ( ; S[i].Parent >= 0; i = S[i].Parent) {}
   return i;
 }
+
+
+typedef int ElementType;
+typedef int SetName;     /* 根结点的下标作为集合名称 */
+typedef ElementType SetType[MAXSIZE];
+
+
+SetName Find(SetType S, ElementType X) {
+  for ( ; S[X] >= 0; X = S[X]) {}
+  return X;
+}
+
+void Union(SetType S, SetName Root1, SetName Root2) {
+  if (Root1 != Root2) {
+    S[Root2] = Root1;    /* Root2的父结点为Root1 */
+  }
+}
