5afded4f9571899f51ae1901e552d0bfaa4ea81a
timtingwei <timtingwei@hotmail.com>
2019-01-12 18:22:49 +0800

    Create: 比较两棵二叉搜索树是否相同

---
+++ zju_dsac/exercise/04-4_CompareBST.c
@@ -0,0 +1,84 @@
+/* Copyright [2019] <mituh> */
+/* 04-4_CompareBST.c */
+/* 比较两棵二叉搜索树是否相同 */
+
+/* 二叉树的存储 */
+
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct TNode *PtrToTNode;
+typedef PtrToTNode Tree;
+struct TNode {
+  int Data;
+  Tree Left, Right;
+};
+
+
+/* 向一棵二叉搜索树中插入元素 */
+Tree Insert(int Elem, Tree T) {
+  if (!T) {
+    T = (Tree)malloc(sizeof(struct TNode));
+    T->Data = Elem;
+    T->Left = NULL; T->Right = NULL;
+  } else {
+    if (Elem < T->Data) {
+      T->Left = Insert(Elem, T->Left);
+    } else if (Elem > T->Data) {
+      T->Right = Insert(Elem, T->Right);
+    }
+    /* else 相等不需要任何其余操作 */
+  }
+  return T;
+}
+
+/* 比较两棵树是否相同 */
+int Compare(Tree T1, Tree T2) {
+  if (T1->Data == T2->Data) {
+    return (Compare(T1->Left, T2->Left)
+      && Compare(T1->Right, T2->Right));
+  } else {
+    return 0;
+  }
+}
+
+
+int main() {
+  int N, K, i, Elem, ok, flag;
+  flag = 0;
+  // while (scanf("%d", &N) == 1 && (!N)) {
+  for (;;) {
+    scanf("%d", &N);
+    if (!N) break;
+    printf("N = %d\n", N);
+    Tree T;
+    scanf("%d", &K);
+    for (i = 0; i < N; i++) {
+      scanf("%d", &Elem);
+      Insert(Elem, T);
+    }
+    while (K--) {
+      Tree CmpT;
+      for (i = 0; i < N; i++) {
+        scanf("%d", &Elem);
+        Insert(Elem, CmpT);
+      }
+      ok = Compare(T, CmpT);
+      if (!flag) {
+        flag = 1;
+      } else {
+        printf("\n");
+      }  /* 控制格式 */
+
+      if (ok) {
+        printf("Yes");
+      } else {
+        printf("No");
+      }
+    }
+  }
+
+  return 0;
+}
+
+
+
