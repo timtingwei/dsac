0e0cd50316466270d0f697124c66ed51e9253914
timtingwei <timtingwei@hotmail.com>
2019-02-21 20:21:33 +0800

    Creaet: 原始递增序列实现希尔排序

---
+++ zju_dsac/ch9/Shell_Sort.c
@@ -0,0 +1,20 @@
+/* Copyright [2019] <mituh> */
+/* Shell_Sort.c */
+/* 原始增量序列的希尔排序 */
+
+/* 间隔的插入排序 */
+
+void Shell_Sort(ElementType A[], int N) {
+  int i, P, D;
+  ElementType Tmp;
+
+  for (D = N/2; D > 0; D /= 2) {   /* 原始希尔递增序列 */
+    for (P = D; P < N; P++) {      /* 插入排序 */
+      Tmp = A[P];
+      for (i = P; i >= D && Tmp < A[i-D]; i -= D) {
+        A[i] = A[i-D];
+      }
+      A[i] = Tmp;
+    }
+  }
+}
