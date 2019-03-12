da44b95a1ad64fab69f3de26c860227db5509589
timtingwei <timtingwei@hotmail.com>
2019-03-12 17:01:13 +0800

    Update: 测试rand()函数

--- ReviewGuideOfDSAC/ch7/test.c
+++ ReviewGuideOfDSAC/ch7/test.c
@@ -1,9 +1,13 @@
 /* Copyright [2019] <mituh> */
 /* test.c */
 /* 生成随机数 */
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 int main() {
   int a;
-  a = rand();
+  srand((unsigned)time(NULL));
+  a = rand()%100+20;
+  printf("a = %d\n", a);
   return 0;
 }
