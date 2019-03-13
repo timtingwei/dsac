d7748dceb38906f0534917626017d22eafc27fdb
timtingwei <timtingwei@hotmail.com>
2019-03-13 22:30:57 +0800

    Update: 计数排序, 限制两个记录之间只进行一次比较

--- ReviewGuideOfDSAC/ch7/7.6/3.c
+++ ReviewGuideOfDSAC/ch7/7.6/3.c
@@ -28,3 +28,24 @@ void Count_Sort(ElementType A[], int N) {
 
 
 /* 两个记录之间只进行一次比较 */
+void Count_Sort(ElementType A[], int N) {
+  ElementType *B = (ElementType*)malloc(sizeof(Element)*N);
+  for (i = 0; i < N; i++) {
+    A[i].count = 0;
+  }
+  for (i = 0; i < N; i++) {
+    for (j = i+1; j < N; j++) {
+      if (A[i].key < A[j].key) {
+        A[j].count++;
+      } else {
+        A[i].count++;
+      }
+    }
+  }
+  for (i = 0; i < N; i++) {
+    B[A[i].count].key = A[i].key;
+  }
+  for (i = 0; i < N; i++) {
+    A[i].key = B[i].key;
+  }
+}
