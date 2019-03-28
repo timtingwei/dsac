99e4603094ad99bd2d120ae479a4d96c9eb17d4c
timtingwei <timtingwei@hotmail.com>
2019-03-28 13:36:07 +0800

    Update: 后序遍历二叉树的非递归版本, 需要记忆理解的标准版本

--- ReviewGuideOfDSAC/ch4/4.3/3.c
+++ ReviewGuideOfDSAC/ch4/4.3/3.c
@@ -2,6 +2,35 @@
 /* 4.3/3.c */
 /* 后序遍历二叉树的非递归版本 */
 
+/* 需要记忆理解的标准版本 */
+void PostOrder(BinTree T) {
+  /* 用栈实现 */
+  Stack S;
+  BinTree p, r;
+  InitStack(S);
+  p = T; r = NULL;
+
+  while (p || !IsEmpty(S)) {
+    if (p) {
+      push(S, p);
+      p = p->lchild;
+    } else {
+      GetTop(S, p);
+      if (p->rchild != NULL && p->rchild != r) {
+        /* 右子树存在且未被访问过 */
+        p = p->rchild;
+        push(S, p);
+        p = p->lchild;      /* 交给第一个if向左走 */
+      } else {
+        pop(S, p);
+        Visit(p->data);
+        r = p; p = NULL;    /* p置为空后, 不向左走, 进入第一个else向右或pop */
+      }  // else
+    }  // else
+  }  // while
+}
+
+/* 我的标准版本  */
 void PostOrder_nonrecuir(BinTree T) {
   /* 二叉树初始每个结点的访问属性设置成0 */
   /* Stack<TreeNode> S; 栈没有属性的? */
