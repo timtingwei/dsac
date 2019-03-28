78a0df2722b9510e3f2aa92ba509c512ee8eb4ed
timtingwei <timtingwei@hotmail.com>
2019-03-28 21:00:13 +0800

    Create: 孩子兄弟表示法存储的森林的叶子结点数

---
+++ ReviewGuideOfDSAC/ch4/4.4/5.c
@@ -0,0 +1,35 @@
+/* Copyright [2019] <mituh> */
+/* 4.4/5.c */
+/* 孩子兄弟表示法存储的森林的叶子结点数 */
+
+/* 孩子-兄弟表示法的树的存储 */
+typedef struct CSNode {
+  ElemenetType data;
+  struct CSNode *firstchild, *nextsibling;
+} CSNode, *CSTree;
+
+/* 由树构成的森林的存储 */
+typedef struct FNode {
+  CSNode Trees[MaxSize];
+  int n;
+} FNode, *Forest;
+
+/* 计算一棵树上的叶子的数量 */
+int GetTreeLeaves(CSTree T) {
+  if (T == NULL) return 0;    /* 空树 */
+  if (T->firstchild == NULL && T->nextsibling == NULL) {
+    return 1;
+  }
+  /* 递归: 一棵树总的叶结点, 左子树 + 右子树叶结点 */
+  return GetTreeLeaves(T->firstchild) + GetTreeLeaves(T->nextsibling);
+}
+
+int CountForestLeaves(Forest F) {
+  /* 计算森林中的叶子个数 */
+  int cnt, i;
+  cnt = 0;
+  for (i = 0; i < F->n; i++) {
+    cnt += GetTreeLeaves(F->Trees[i]);
+  }
+  return cnt;
+}
